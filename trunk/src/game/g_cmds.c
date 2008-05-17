3861452893a000c5ebe080c11d369b06a6f78b25
Paradox460 <Paradox460@e6965c97-d842-0410-82ac-ab76856a6688>
2008-05-17 02:55:38 +0000

    Fixed missing return in PTRC commands, now they work properly.

--- trunk/src/game/g_cmds.c
+++ trunk/src/game/g_cmds.c
@@ -4248,6 +4248,7 @@ void Cmd_PTRCVerify_f( gentity_t *ent )
       trap_SendServerCommand( ent - g_entities,
         "print \"This PTR code is already in use\n\"" );
       G_AdminsPrintf("%s^7 attempted to use %s^7's PTR code\n", ent->client->pers.netname, players->client->pers.netname);
+      return;
     }
   }
 
@@ -4314,6 +4315,7 @@ void Cmd_PTRCRestore_f( gentity_t *ent )
       trap_SendServerCommand( ent - g_entities,
         "print \"This PTR code is already in use\n\"" );
       G_AdminsPrintf("%s^7 attempted to use %s^7's PTR code\n", ent->client->pers.netname, players->client->pers.netname);
+      return;
     }
   }
   
